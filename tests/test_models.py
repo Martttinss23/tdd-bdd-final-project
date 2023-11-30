b3032519f69022db2dfb12911f3620733d9da758
Marti Planaguma Sola <marti.planaguma@tttech-auto.com>
2023-11-30 14:42:03 +0100

    SPTICOMA-954: Add 'Test Case to Find a Product by Category'
    Signed-off-by: Marti Planaguma Sola <marti.planaguma@tttech-auto.com>

--- tests/test_models.py
+++ tests/test_models.py
@@ -213,3 +213,22 @@ class TestProductModel(unittest.TestCase):
         # 6.Assert that each product's availability matches the expected availability.
         for product in found_products:
             self.assertEqual(product.available, product_available)
+
+    def test_find_product_by_category(self):
+        """It should Find Products by Category"""
+        # 1.Create a batch of 10 Product objects using the ProductFactory and save them to the database.
+        nr_products = 10
+        products = ProductFactory.create_batch(nr_products)
+        for product in products:
+            product.create()
+        # 2.Retrieve the category of the first product in the products list
+        product_category = products[0].category
+        # 3.Count the number of occurrences of the product that have the same category in the list.
+        nr_ocurrences = len([product for product in products if product.category == product_category])
+        # 4.Retrieve products from the database that have the specified category.
+        found_products = Product.find_by_category(product_category)
+        # 5.Assert if the count of the found products matches the expected count.
+        self.assertEqual(found_products.count(), nr_ocurrences)
+        # 6.Assert that each product's category matches the expected category.
+        for product in found_products:
+            self.assertEqual(product.category, product_category)
